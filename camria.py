2433f9b02c6afb76c38802a1283929d20c1a1cc4
T1murKO <kutsenko.timur@gmail.com>
2024-03-30 21:31:27 +0200

    *

--- camria.py
+++ camria.py
@@ -1086,7 +1086,7 @@ def clear_chat(driver):
 
 def set_zoom_level(zoom=0.5):
     driver.get("chrome://settings/appearance")
-    sleep(3)
+    sleep(60)
     script = f"""
     let settingsUiShadowRoot = document.querySelector('settings-ui').shadowRoot;
     let settingsMainShadowRoot = settingsUiShadowRoot.querySelector('settings-main').shadowRoot;
